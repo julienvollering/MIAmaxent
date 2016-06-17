67788dd6faa7bdcbacf796004f519737738762e1
Julien Vollering <julien.vollering@hisf.no>
2016-06-17 15:13:12 +0200

    .binaryrvcheck moved to utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,26 @@
+#' checks the validity of RV values
+#'
+#' Presence-only data should be coded as: 1/NA (preferred) or 1/0 (danger of
+#' misinterpretation as presence/absence data)
+#'
+#' @param rv Vector of response variable values
+
+.binaryrvcheck <- function(rv) {
+  if (length(levels(as.factor(rv))) > 2) {
+    stop("The response variable must contain 2 levels only: presence (1)
+      and background (NA/0)", call. = FALSE)
+  }
+  if (anyNA(rv) && length(levels(as.factor(rv))) > 1) {
+    stop("The response variable must contain 2 levels only: presence (1)
+      and background (NA/0)", call. = FALSE)
+  }
+  if (class(rv) != "numeric" && class(rv) != "integer") {
+    stop("The response variable must be numeric or integer class: presence (1)
+      and background (NA/0)", call. = FALSE)
+  }
+  }
+
+
 #' Best prefix match.
 #'
 #' Return only the string in 'a' which best matches any of the strings in 'b'
@@ -8,7 +31,7 @@
 #' @return single character string
 
 
-best.match <- function(a, b) {
+.best.match <- function(a, b) {
   score <- sapply(a, function(a) {
     reg <- regexpr(a, b)
     max(attr(reg, "match.length"))
