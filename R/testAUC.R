d5e8b1180fefd1896145b840a54b51a751995b78
Julien Vollering <julien.vollering@hisf.no>
2016-06-24 13:48:57 +0200

    testAUC now independent of .jar

--- R/testAUC.R
+++ R/testAUC.R
@@ -8,26 +8,41 @@
 #'
 #' \code{DESCRIPTION Imports}: PresenceAbsence
 #'
-#' @param predictions Pathway to the "backgroundPredictions.csv" file of the
-#'   model in question.
-#' @param test Vector of test data (1/0/NA), with identical length and order as
-#'   training data.
+#' @param data Data frame containing test data in the first column and
+#'   corresponding explanatory variables in subsequent columns. The test data
+#'   should be coded as: 1/0/NA, representing presence, absence, and unknown.
+#' @param transformation Pathway to the .Rdata file containing the
+#'   transformations used to build the model. This file is saved as a result of
+#'   the \code{\link{deriveVars}} function. Alternatively, a list object
+#'   containing all of the named transformations (e.g. the second item in the
+#'   list returned by \code{\link{deriveVars}}).
+#' @param model Pathway to the .lambdas file of the model in question. This file
+#'   is saved as a result of the \code{selectEV} function.
 #'
 #' @export
 
 
-testAUC <- function(predictions = NULL, test) {
+testAUC <- function(data, transformation, model) {
 
-  logistic <- read.csv(predictions)[,5]
-  df <- data.frame(1:length(logistic), test, logistic)
-  colnames(df) <- c("ID", "test", "logistic")
-  testdf <- na.omit(df)
-  orderedpreds <- df$logistic[order(df$logistic)]
+  data <- na.omit(data)
+  test <- data[, 1]
+  PRO <- projectModel(data, transformation, model)[[1]][, 1]
+  rangedoutput <- (PRO - min(PRO)) / diff(range(PRO))
+  PRO1 <- (1 - min(PRO)) / diff(range(PRO))
+  df <- data.frame(ID = 1:nrow(data), test = test, output = rangedoutput)
+
+  AUC <- PresenceAbsence::auc(df, st.dev = FALSE)
+
+  orderedpreds <- df$output[order(df$output)]
   breaks <- orderedpreds[-length(orderedpreds)] + diff(orderedpreds)/2
+  PresenceAbsence::auc.roc.plot(df, threshold = breaks, add.legend = FALSE,
+    line.type = FALSE, color = "red")
 
-  AUC <- PresenceAbsence::auc(testdf, st.dev = FALSE, na.rm=T)
-  suppressWarnings(PresenceAbsence::auc.roc.plot(df, threshold = breaks, na.rm = TRUE,
-    add.legend = FALSE, line.type = FALSE, color = "red"))
+  PRO1thresh <- PresenceAbsence::roc.plot.calculate(df, PRO1)
+  x <- 1 - PRO1thresh$specificity
+  y <- PRO1thresh$sensitivity
+  points(x, y, pch = 19, col = "#999999")
+  text(x, y, "PRO = 1", pos = 3, col = "#999999", cex = 0.9)
 
   return(AUC)
 }
