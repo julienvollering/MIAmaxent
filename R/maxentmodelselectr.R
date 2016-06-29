d0fbbe8838c473696d198e375d6192f994cd89b0
Julien Vollering <julien.vollering@hisf.no>
2016-06-29 12:09:10 +0200

    added package description to pkg index and readme

---
+++ R/maxentmodelselectr.R
@@ -0,0 +1,31 @@
+#' maxentmodelselectr: Maxent Distribution Model Selection.
+#'
+#' Training, selecting, and evaluating maximum entropy (Maxent) distribution
+#' models. This package provides tools for user-controlled transformation of
+#' explanatory variables, selection of variables by nested model comparison, and
+#' flexible model evaluation and projection. The methods implemented here are
+#' based on the strict maximum likelihood interpretation of maximum entropy
+#' modelling (Halvorsen, 2013, Halvorsen et al., 2015).
+#'
+#' The predecessor to this package is the MIA Toolbox, which is described in
+#' detail in Mazzoni et al. (2015).
+#'
+#' The maximum entropy algorithm utilized in this package is provided by the
+#' MaxEnt java program (Phillips et al., 2006).
+#'
+#' @references Halvorsen, R. (2013) A strict maximum likelihood explanation of
+#'   MaxEnt, and some implications for distribution modelling. Sommerfeltia, 36,
+#'   1-132.
+#' @references Halvorsen, R., Mazzoni, S., Bryn, A. & Bakkestuen, V. (2015)
+#'   Opportunities for improved distribution modelling practice via a strict
+#'   maximum likelihood interpretation of MaxEnt. Ecography, 38, 172-183.
+#' @references Mazzoni, S., Halvorsen, R. & Bakkestuen, V. (2015) MIAT: Modular
+#'   R-wrappers for flexible implementation of MaxEnt distribution modelling.
+#'   Ecological Informatics, 30, 215-221.
+#' @references Phillips, S.J., Anderson, R.P. & Schapire, R.E. (2006) Maximum
+#'   entropy modeling of species geographic distributions. Ecological Modelling,
+#'   190, 231-259.
+#'
+#' @docType package
+#' @name maxentmodelselectr
+NULL
