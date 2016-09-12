d9145ba572fd0f490b403b2c4e5074c91d28cc38
Julien Vollering <julien.vollering@hisf.no>
2016-09-12 10:59:09 +0200

    dont run with spline transformations

--- R/deriveVars.R
+++ R/deriveVars.R
@@ -69,10 +69,9 @@
 #'    dir = "D:/path/to/modeling/directory")
 #' }
 #'
-#' DVs <- deriveVars(sp1po)
+#' DVs <- deriveVars(sp1po, transformtype = c("L", "M", "D", "B"))
 #' str(DVs$EVDV)
 #' summary(DVs$transformations)
-#' plot(sp1po$EV11, DVs$EVDV$EV11$EV11_HF8)
 #'
 #' \dontrun{
 #' # From vignette:
