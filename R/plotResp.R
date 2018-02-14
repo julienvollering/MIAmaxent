9ea2fc787537d8e90ea9fa8d6f69532ae1411a1a
Julien Vollering <julienvollering@gmail.com>
2018-02-14 13:57:32 +0100

    plotResp with iwlr -- in progress

--- R/plotResp.R
+++ R/plotResp.R
@@ -12,7 +12,11 @@
 #' data points, as well as a line, representing an exponentially weighted moving
 #' average of the model response over intervals of the EV.
 #'
-#' The \code{EV} specified in \code{dvdata} must not be an interaction term.
+#' Note that the rows in \code{data} and \code{dvdata} must correspond to the
+#' same observations, so that the transformed data relate correctly to their
+#' untransformed values. This will automatically be the case if
+#' \code{\link{deriveVars}} and \code{\link{selectDVforEV}} are used to create
+#' \code{dvdata} from \code{data}.
 #'
 #' @param data Data frame containing the response variable in the first column
 #'   and explanatory variables in subsequent columns. The response variable
@@ -22,10 +26,9 @@
 #'   each list item is a data frame containing \emph{selected} DVs for a
 #'   \emph{selected} EV (e.g. the first item in the list returned by
 #'   \code{\link{selectEV}}).
-#' @param EV Name or list index of the explanatory variable in \code{dvdata} for
-#'   which the response curve is to be generated. Interaction terms not allowed.
-#' @param dir Directory to which files will be written. Defaults to the working
-#'   directory.
+#' @param EV Name of the explanatory variable or its list index in \code{dvdata}
+#'   for which the response curve is to be generated. Interaction terms not
+#'   allowed.
 #' @param logscale Logical. Plot the common logarithm of PRO rather than PRO
 #'   itself.
 #' @param ... Arguments to be passed to \code{plot} to control the appearance of
@@ -33,57 +36,37 @@
 #'   \code{col} for color \item \code{pch} for type }
 #'
 #' @return In addition to the graphical output, the plotted data is returned. In
-#'   the case of a continuous EV, the plotted data is a list of 2: \enumerate{
-#'   \item \code{respPts}. Model response across individual data points. Columns
-#'   in this data frame represent the following: EV value ("EV"), Probability
-#'   Ratio Output of the model ("PRO"), and corresponding EV interval ("int").
-#'   \item \code{respLine}. Model response across intervals of the EV. Columns
-#'   in this data frame represent the following: EV interval ("int"), number of
-#'   points in the interval ("n"), mean EV value of the points in the interval
-#'   ("intEV"), mean Probability Ratio Output of the points in the interval
-#'   ("intPRO"), and exponentially weighted moving average of intPRO
-#'   ("smoothPRO").}
+#'   the case of a continuous EV, the plotted data consists of: EV values ("EV")
+#'   and Probability Ratio Output of the model ("PRO").
 #'
 #'   In the case of a categorical EV, the plotted data is a data frame
 #'   containing the number of points in the level ("n"), the level name
-#'   ("level"), and the mean Probability Ratio Output of the level ("levelRV").
+#'   ("level"), and the mean Probability Ratio Output of the level ("levelPRO").
 #'
 #' @examples
-#' \dontrun{
-#' responseEV1 <- plotResp(dat, deriveddat, "EV1", dir = "D:/path/to/modeling/directory")
-#'
-#' # From vignette
-#' pr_bygallResp <- plotResp(grasslandPO, grasslandEVselect[[1]], "pr_bygall")
-#' par(mfrow = c(1,2))
-#' pr_bygallFOP <- plotFOP(grasslandPO, "pr_bygall", intervals=50)
-#' pr_bygallResp <- plotResp(grasslandPO, grasslandEVselect[[1]], "pr_bygall")
-#' par(mfrow = c(1,1))
-#' }
 #'
 #' @export
 
 
-plotResp <- function(data, dvdata, EV, dir = NULL, logscale = FALSE, ...) {
+plotResp <- function(data, dvdata, EV, logscale = FALSE, ...) {
 
   .binaryrvcheck(data[, 1])
 
-  if (is.null(dir)) { dir <- getwd()}
-
-  fdir <- file.path(dir, "plotResp")
-  dir.create(fdir, showWarnings = FALSE, recursive = TRUE)
   evname <- names(dvdata[EV])
-  if (!(evname %in% colnames(data))) {
-    stop("The specified EV must be present in the untransformed data.
+  if (!(evname %in% colnames(data) && evname %in% names(dvdata))) {
+    stop("The specified EV must be present in the both untransformed 'data' and the transformed 'dvdata'.
        E.g. interaction terms between multiple EVs are not supported. \n ",
       call. = FALSE)
   }
-  modeldir <- file.path(fdir, paste0("response", evname))
-  dir.create(modeldir, showWarnings = FALSE)
 
-  .runjar(data[, 1], dvdata[[EV]], maxbkg = nrow(data) + 1, modeldir)
+  df <- data.frame("RV"=data[,1], dvdata[[evname]])
+  formula <- stats::formula(paste("RV ~", paste(names(df)[-1], collapse=" + ")))
+  model <- .runIWLR(formula, df)
+  df[,1][is.na(df[,1])] <- 0
+  newdata <- model.matrix(stats::update(formula, ~. -1), df)
+  raw <- exp((newdata %*% model$betas) + model$alpha)
 
-  output <- utils::read.csv(file.path(modeldir, "1_backgroundPredictions.csv"))
-  respPts <- data.frame(EV = data[, evname], PRO = output[,3]*length(output[,3]))
+  respPts <- data.frame(EV = data[, evname], PRO = raw*nrow(df))
   if (logscale == TRUE) {respPts$PRO <- log10(respPts$PRO)}
 
   if (class(respPts[, 1]) %in% c("numeric", "integer")) {
@@ -92,27 +75,10 @@ plotResp <- function(data, dvdata, EV, dir = NULL, logscale = FALSE, ...) {
       ylab = ifelse(logscale == TRUE, "log Probability Ratio Output (logPRO)",
         "Probability Ratio Output (PRO)"))
 
-    if (nrow(respPts) >= 250) {
-      intervals <- min(c(ceiling(nrow(respPts) / 50), 100))
-      respPts$int <- cut(respPts[, 1], max(2, intervals))
-      grouped <- dplyr::group_by(respPts, int)
-      respLine <- as.data.frame(dplyr::summarise(grouped, n = n(),
-        intEV = mean(EV, na.rm = TRUE),
-        intPRO = mean(PRO, na.rm = TRUE)))
-      respLine$smoothPRO <- .ewma(respLine$intPRO, 5)
-      graphics::lines(respLine$smoothPRO ~ respLine$intEV, col="red", lwd = 2)
-      result <- list("respPts" = respPts, "respLine" = respLine)
-    } else {
-      result <- respPts
-      warning("Exponentially weighted moving average line not drawn due to few intervals",
-        call. = FALSE)
-    }
+    if (logscale == TRUE) { graphics::abline(h = 0, lty = 3) }
+    else { graphics::abline(h = 1, lty = 3) }
 
-    if (logscale == TRUE) {
-      graphics::abline(h = 0, lty = 3)
-    } else {
-      graphics::abline(h = 1, lty = 3)
-    }
+    result <- respPts
   }
 
   if (class(respPts[, 1]) %in% c("factor", "character")) {
