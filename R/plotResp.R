f84b8cf9c0e07d44802863cd574f09863777b001
Julien Vollering <julienvollering@gmail.com>
2018-02-15 12:38:33 +0100

    brand new plotResp using iwlr

--- R/plotResp.R
+++ R/plotResp.R
@@ -1,101 +1,95 @@
 #' Plot single-effect model response.
 #'
-#' \code{plotResp} plots the single-effect response of a given Maxent model over
-#' any of the included explanatory variables (EVs) in that model. For
-#' categorical variables, a bar plot is returned rather than a scatter plot.
-#' \code{plotResp} also returns a data frame containing the plotted data (for
-#' customizable graphics). Single-effect response curves present the response of
-#' a model containing the explanatory variable of interest only (cf.
-#' marginal-effect response curves; \code{\link{plotResp2}}).
+#' \code{plotResp} plots the single-effect response of a given model over any of
+#' the explanatory variables (EVs) included in that model. For categorical
+#' variables, a bar plot is returned rather than a scatter plot. Single-effect
+#' response curves present the response of a model containing the explanatory
+#' variable of interest only (cf. marginal-effect response curves;
+#' \code{\link{plotResp2}}).
 #'
-#' The plot contains points, representing the model response across individual
-#' data points, as well as a line, representing an exponentially weighted moving
-#' average of the model response over intervals of the EV.
-#'
-#' Note that the rows in \code{data} and \code{dvdata} must correspond to the
-#' same observations, so that the transformed data relate correctly to their
-#' untransformed values. This will automatically be the case if
-#' \code{\link{deriveVars}} and \code{\link{selectDVforEV}} are used to create
-#' \code{dvdata} from \code{data}.
-#'
-#' @param data Data frame containing the response variable in the first column
-#'   and explanatory variables in subsequent columns. The response variable
-#'   should represent presence/background data, coded as: 1/NA. See
+#' @param data Data frame containing the observations used to train the
+#'   \code{model}, with the response variable in the first column and
+#'   explanatory variables in subsequent columns. The response variable should
+#'   represent presence/background data, coded as: 1/NA. See
 #'   \code{\link{readData}}.
-#' @param dvdata List of explanatory variables used to train the model, where
-#'   each list item is a data frame containing \emph{selected} DVs for a
-#'   \emph{selected} EV (e.g. the first item in the list returned by
-#'   \code{\link{selectEV}}).
-#' @param EV Name of the explanatory variable or its list index in \code{dvdata}
-#'   for which the response curve is to be generated. Interaction terms not
+#' @param transformations Transformation functions used to create the derived
+#'   variables in the model. I.e. the 'transformations' returned by
+#'   \code{\link{deriveVars}}. Equivalently, the full file pathway of the
+#'   'transformations.Rdata' file saved as a result of \code{\link{deriveVars}}.
+#' @param model The model for which the response is to be plotted, represented
+#'   by an object of class 'glm'. This may be the object returned by
+#'   \code{\link{chooseModel}}, or the 'selectedmodel' returned by
+#'   \code{\link{selectEV}}.
+#' @param EV Name of the explanatory variable or its column index in \code{data}
+#'   for which the response curve is to be plotted. Interaction terms not
 #'   allowed.
 #' @param logscale Logical. Plot the common logarithm of PRO rather than PRO
 #'   itself.
-#' @param ... Arguments to be passed to \code{plot} to control the appearance of
-#'   the plot. For example: \itemize{ \item \code{cex} for size of points \item
-#'   \code{col} for color \item \code{pch} for type }
-#'
-#' @return In addition to the graphical output, the plotted data is returned. In
-#'   the case of a continuous EV, the plotted data consists of: EV values ("EV")
-#'   and Probability Ratio Output of the model ("PRO").
-#'
-#'   In the case of a categorical EV, the plotted data is a data frame
-#'   containing the number of points in the level ("n"), the level name
-#'   ("level"), and the mean Probability Ratio Output of the level ("levelPRO").
+#' @param ... Arguments to be passed to \code{plot} or \code{barplot} to control
+#'   the appearance of the plot. For example: \itemize{ \item \code{lwd} for
+#'   line width \item \code{cex.main} for size of plot title \item \code{space}
+#'   for space between bars }
 #'
 #' @examples
 #'
 #' @export
 
 
-plotResp <- function(data, dvdata, EV, logscale = FALSE, ...) {
+plotResp <- function(data, transformations, model, EV, logscale = FALSE, ...) {
 
   .binaryrvcheck(data[, 1])
 
-  evname <- names(dvdata[EV])
-  if (!(evname %in% colnames(data) && evname %in% names(dvdata))) {
-    stop("The specified EV must be present in the both untransformed 'data' and the transformed 'dvdata'.
-       E.g. interaction terms between multiple EVs are not supported. \n ",
-      call. = FALSE)
+  if (EV==1) {
+    stop("'EV' cannot be the first column of 'data', which must be the response variable")
   }
 
-  df <- data.frame("RV"=data[,1], dvdata[[evname]])
-  formula <- stats::formula(paste("RV ~", paste(names(df)[-1], collapse=" + ")))
-  model <- .runIWLR(formula, df)
-  df[,1][is.na(df[,1])] <- 0
-  newdata <- model.matrix(stats::update(formula, ~. -1), df)
-  raw <- exp((newdata %*% model$betas) + model$alpha)
+  evname <- names(data[EV])
+  betas <- model$betas[grep(paste0(evname, "_"), names(model$betas))]
+  if (length(betas)==0) {
+    stop("The 'EV' specified is not in the model")
+  }
 
-  respPts <- data.frame(EV = data[, evname], PRO = raw*nrow(df))
-  if (logscale == TRUE) {respPts$PRO <- log10(respPts$PRO)}
+  alltransf <- .load.transf(transformations)
+  evtransfs <- alltransf[match(paste0(names(betas), "_transf"),
+                               names(alltransf), nomatch = 0)]
+  if (!(length(evtransfs)==length(betas))) {
+    stop("The transformation function for at least one DV in the model is missing")
+  }
 
-  if (class(respPts[, 1]) %in% c("numeric", "integer")) {
-    graphics::plot(respPts[, 2] ~ respPts[, 1], ...,
+  dvdata <- lapply(evtransfs, function(f, x) { f(x) }, x=data[,evname])
+  names(dvdata) <- names(betas)
+  traindata <- data.frame("RV"=data[,1], dvdata)
+  formula <- stats::formula(paste("RV ~", paste(names(betas), collapse = " + ")))
+  smodel <- .runIWLR(formula, traindata)
+
+  if (class(data[, evname]) %in% c("numeric", "integer")) {
+    seq <- seq(min(data[,evname]), max(data[,evname]), length.out = 100)
+  }
+  if (class(data[, evname]) %in% c("factor", "character")) {
+    seq <- unique(data[, evname])
+  }
+  newdata <- do.call(cbind, lapply(evtransfs, function(f, x) { f(x) }, x=seq))
+  colnames(newdata) <- names(betas)
+  raw <- exp((newdata %*% smodel$betas) + smodel$alpha)
+
+  resp <- data.frame(EV = seq, PRO = raw*nrow(data))
+  if (logscale == TRUE) {resp$PRO <- log10(resp$PRO)}
+
+  if (class(resp[, 1]) %in% c("numeric", "integer")) {
+    graphics::plot(resp[, 2] ~ resp[, 1], type="l", col="red", ...,
       main = paste0("Single-effect response plot: ", evname), xlab = evname,
       ylab = ifelse(logscale == TRUE, "log Probability Ratio Output (logPRO)",
         "Probability Ratio Output (PRO)"))
-
-    if (logscale == TRUE) { graphics::abline(h = 0, lty = 3) }
-    else { graphics::abline(h = 1, lty = 3) }
-
-    result <- respPts
   }
 
-  if (class(respPts[, 1]) %in% c("factor", "character")) {
-    respBar <- as.data.frame(dplyr::summarise(dplyr::group_by(respPts, EV),
-      n = n(), levelPRO = mean(PRO, na.rm = TRUE)))
-    graphics::barplot(respBar[, 3], names.arg = respBar[, 1], ...,
+  if (class(resp[, 1]) %in% c("factor", "character")) {
+    graphics::barplot(resp[, 2], names.arg = resp[, 1], col="red", ...,
       main = paste0("Single-effect response plot: ", evname), xlab = evname,
       ylab = ifelse(logscale == TRUE, "log Probability Ratio Output (logPRO)",
         "Probability Ratio Output (PRO)"))
-    if (logscale == TRUE) {
-      graphics::abline(h = 0, lty = 3)
-    } else {
-      graphics::abline(h = 1, lty = 3)
-    }
-    result <- data.frame(n = respBar[, 2], level = respBar[, 1],
-      levelPRO = respBar[, 3])
   }
 
-  return(result)
+  if (logscale == TRUE) { graphics::abline(h = 0, lty = 3)
+  } else { graphics::abline(h = 1, lty = 3) }
+
 }
